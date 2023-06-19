51c1e6220d095914be903f75f098478d7623bbfc
Henry Lee <hlee.0323@gmail.com>
2023-06-19 14:31:53 +0100

    (progress) made updateStockLevelsController

--- controllers/updateStockLevelsController.js
+++ controllers/updateStockLevelsController.js
@@ -3,8 +3,22 @@ const updateStockLevelsService = require('../services/updateStockLevelsService')
 const updateStockLevels = (request, response) => {
     console.log('Controller: updateStockLevels');
     const SKU = request.params.SKU;
-    const updatedStockLevels = request.query
-    updateStockLevelsService.updateStockLevels(SKU, updatedStockLevels).then((result) => res.send(result));
+
+    updateStockLevelsService
+    .updateStockLevels(SKU)
+    .then(() => {
+        const message = {"message": "Successfully updated stock levels for product."}
+        response.status(200).json(message);
+    })
+    .catch((error) => {
+        let status = 500;
+        const message = {"message": error.message, "data": []}
+
+        if (error.message.startsWith("Invalid")) {
+            status = 400;
+        }
+        response.status(status).send(message);
+    })
 }
 
 module.exports.updateStockLevels = updateStockLevels;
