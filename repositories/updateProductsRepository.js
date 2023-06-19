1255952f2c935a6285375c5beeb7e3ca7f5df4fa
Phildallen <phil_372@live.com>
2023-06-19 10:11:09 +0100

    working, but needs error messages

--- repositories/updateProductsRepository.js
+++ repositories/updateProductsRepository.js
@@ -1,29 +1,48 @@
 const dbService = require('../db/dbService');
-//WHERE doesn't pick up if sku is invalid. needs check
 const updateProducts = async (sku,updatedDetails) => {
     console.log('Repository: updateProducts');
     const connection = await dbService.createConnection()
 
-    const name = updatedDetails.name;
-    const price= updatedDetails.price;
+    const name = updatedDetails.updatedData.name;
+    const price= updatedDetails.updatedData.price;
     console.log(sku)
 
-    const sql = 'UPDATE products SET name = ?, price = ? WHERE sku = ?';
-    const values = [name, price, sku];
+    const selectSql = 'SELECT * FROM products WHERE sku = ?';
+    const selectValues = [sku];
 
-    return new Promise((resolve, reject) => {
-        connection.query(sql, values, (err, results) => {
-            if (err) {
-                console.error('Error updating name and price:', err);
-                reject(err);
+    connection.query(selectSql, selectValues, (selectErr, selectResults) => {
+        if (selectErr) {
+            console.error('Error checking SKU:', selectErr);
+            connection.end();
+            return;
+        }
+
+        if (selectResults.length === 0) {
+            // const error = new Error('SKU not found in the database');
+            // console.error('Error:', error);
+            // connection.end();
+            // throw error;
+            let status = 500;
+            const message = {"message": error.message, "data": []}
+
+        }
+
+        // The SKU exists, so proceed with the update
+        const updateSql = 'UPDATE products SET name = ?, price = ? WHERE sku = ?';
+        const updateValues = [name, price, sku];
+
+        connection.query(updateSql, updateValues, (updateErr, updateResults) => {
+            if (updateErr) {
+                console.error('Error updating name and price:', updateErr);
+                connection.end();
                 return;
             }
+
             console.log('Name and price updated successfully');
-            resolve(results);
+            connection.end();
+            resolve(updateResults);
         });
     });
-
-    // return connection.query('UPDATE products SET name WHERE sku=sku')
-}
+};
 
 module.exports.updateProducts = updateProducts;
