144ca0b8e2a5b1ae3b8fe69ff77c9165b7db2507
Henry Lee <hlee.0323@gmail.com>
2023-06-19 14:35:34 +0100

    (progress) updateStockLevelsServices doesn't work yet

--- services/updateStockLevelsService.js
+++ services/updateStockLevelsService.js
@@ -1,8 +1,25 @@
 const updateStockLevelsRepository = require('../repositories/updateStockLevelsRepository');
+const SKUToID = require('../functions/sku-to-id');
+const getStockLevelsService = require('../services/getStockLevelsService');
 
-const updateStockLevels = async () => {
+const updateStockLevels = async (SKU) => {
+
     console.log('Service: updateStockLevels');
-    // some code here pls
+    if(!SKU.startsWith("ICBWHTIS") || SKU.length > 12) {
+        const message = "Invalid SKU";
+        throw new Error(message);
+    }
+
+    const id = SKUToID.SKUToId(SKU)
+
+    try {
+        const stockLevels = await getStockLevelsService.getStockLevels(id);
+        console.log(typeof(stockLevels));
+        // return await getStockLevelsService.getStockLevels(id)
+    } catch {
+        const message = "Unexpected error"
+        throw new Error(message)
+    }
 }
 
 module.exports.updateStockLevels = updateStockLevels;
