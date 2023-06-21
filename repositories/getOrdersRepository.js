761b5e43413cc8661d7b5aedbab174a937edb603
Phildallen <phil_372@live.com>
2023-06-21 14:56:50 +0100

    compl

--- repositories/getOrdersRepository.js
+++ repositories/getOrdersRepository.js
@@ -1,54 +1,11 @@
 const dbService = require('../db/dbService');
-const {idToSKU} = require("../functions/id-to-sku");
-const {orderDBToJson} = require("../functions/orderDB-to-json")
-
 
 const getOrders = async () => {
     console.log('Repository: getOrders');
-
-
-
     const connection = await dbService.createConnection()
-    const results = await connection.query('SELECT `id`, `order_number`, `customer_email`, `order` FROM `orders`;');
+    const results = await connection.query('SELECT `id`, `order_number`, `customer_email`, `products` FROM `orders`;');
 
-    // turn results into array of [id's, quantities]
-    let resultsArray = [];
-    let productsArray = [];
-
-    results.forEach((result) => {
-        const orderInfo = result.order.split("(").join("").split(")").join("");
-        const orderArrayHalf = orderInfo.split(",");
-        // get the id's out of the array in this for each loop, then pull out their names also
-        const eachOrder = []
-        orderArrayHalf.forEach((entry) => {
-            eachOrder.push(entry.split(":"))
-        })
-        resultsArray.push(result.order_number,result.customer_email,eachOrder)
-    })
-    console.log(resultsArray)
-
-    // resultsArray.forEach((array) => {
-    //     console.log(array)
-    //     const getOrderDetails = (array) => {
-    //         const connection = dbService.createConnection();
-    //         const id = array[0];
-    //         console.log(id)
-    //         const sql = 'SELECT `SKU`, `name` FROM `products` WHERE `id`=?';
-    // const values = [id]
-    //         return connection.query(sql, values)
-    //         console.log(getOrderDetails())
-    //     }
-    //
-    // })
-
-    // do a sql to find the name of the items from the products database [ [ id, quantity ], [ id, quantity ] ]
-    // forEach return array[0]
-    // resturn the results
-
-    const returning = {order: resultsArray}
-
-    return returning
-
+    return results
 }
 
 module.exports.getOrders = getOrders;
--- services/getOrdersService.js
+++ services/getOrdersService.js
@@ -3,7 +3,28 @@ const getOrdersRepository = require('../repositories/getOrdersRepository');
 const getOrders = async () => {
     console.log('Service: getOrders');
     try {
-        return await getOrdersRepository.getOrders();
+        const results = await getOrdersRepository.getOrders();
+        let resultsArray = [];
+        let eachOrderObject = {};
+
+        results.forEach((result) => {
+            const orderInfo = result.products.split("(").join("").split(")").join("");
+            const orderArrayHalf = orderInfo.split(",");
+            let resultsObject ={};
+            const eachOrder = [];
+            const keys =["name", "quantity"];
+            orderArrayHalf.forEach((entry) => {
+                eachOrder.push(entry.split(":"));
+                eachOrderObject = eachOrder.map((innerArray) => Object.assign({}, ...innerArray.map((value, index) => ({ [keys[index]]: value}))));
+            })
+
+            resultsObject.order_number = result.order_number;
+            resultsObject.customer_email = result.customer_email;
+            resultsObject.products = eachOrderObject;
+            resultsArray.push(resultsObject);
+        })
+
+        return resultsArray;
     } catch {
         const message = "Unexpected error";
         throw new Error(message);
