f31c8fff3421d600e0c9499beb2c5279a3d43cc1
Phildallen <phil_372@live.com>
2023-06-21 10:44:04 +0100

    commit before merge

--- repositories/getOrdersRepository.js
+++ repositories/getOrdersRepository.js
@@ -1,25 +1,53 @@
 const dbService = require('../db/dbService');
+const {idToSKU} = require("../functions/id-to-sku");
+const {orderDBToJson} = require("../functions/orderDB-to-json")
 
 
 const getOrders = async () => {
     console.log('Repository: getOrders');
 
-    const connection = await dbService.createConnection()
-    const results = connection.query('SELECT `id`, `customer_email`, `customer_name`, `address_line_1`, `address_line_2`,' +
-        ' `address_line_3`, `town_city`, `postcode`, `country`, `order` FROM `orders`;');
-    const modifiedResults = results.map(row => {
-        console.log(10)
-        console.log("10")
-        string = String(row.order)
-        console.log(string)
-        const bob = { orders: [ { id: 4, quantity: 3 } ] }
-        console.log(bob)
-        const stringy2 = JSON.stringify(bob)
-        console.log(stringy2)
-        const modifiedOrders = JSON.stringify(string);
-        return { ...row, order: modifiedOrders }});
 
-    return modifiedResults
+
+    const connection = await dbService.createConnection()
+    const results = await connection.query('SELECT `id`, `order_number`, `customer_email`, `order` FROM `orders`;');
+
+    // turn results into array of [id's, quantities]
+    let resultsArray = [];
+    let productsArray = [];
+
+    results.forEach((result) => {
+        const orderInfo = result.order.split("(").join("").split(")").join("");
+        const orderArrayHalf = orderInfo.split(",");
+        // get the id's out of the array in this for each loop, then pull out their names also
+        const eachOrder = []
+        orderArrayHalf.forEach((entry) => {
+            eachOrder.push(entry.split(":"))
+        })
+        resultsArray.push(result.order_number,result.customer_email,eachOrder)
+    })
+    console.log(resultsArray)
+
+    // resultsArray.forEach((array) => {
+    //     console.log(array)
+    //     const getOrderDetails = (array) => {
+    //         const connection = dbService.createConnection();
+    //         const id = array[0];
+    //         console.log(id)
+    //         const sql = 'SELECT `SKU`, `name` FROM `products` WHERE `id`=?';
+    // const values = [id]
+    //         return connection.query(sql, values)
+    //         console.log(getOrderDetails())
+    //     }
+    //
+    // })
+
+    // do a sql to find the name of the items from the products database [ [ id, quantity ], [ id, quantity ] ]
+    // forEach return array[0]
+    // resturn the results
+
+    const returning = {order: resultsArray}
+
+    return returning
 
 }
 
--- services/getOrdersService.js
+++ services/getOrdersService.js
@@ -1,16 +1,7 @@
 const getOrdersRepository = require('../repositories/getOrdersRepository');
-const {json} = require("express");
 
 const getOrders = async () => {
     console.log('Service: getOrders');
-    const stringy = JSON.stringify({ orders: [ { id: 4, quantity: 3 } ] })
-// console.log(stringy)
-    const bob = { orders: [ { id: 4, quantity: 3 } ] }
-    const stringy2 = JSON.stringify(bob)
-    // console.log(stringy2)
-
-    // const parse = JSON.parse('{"orders":[{"id":4,"quantity":3}]}')
-    // console.log(parse)
     try {
         return await getOrdersRepository.getOrders();
     } catch {
