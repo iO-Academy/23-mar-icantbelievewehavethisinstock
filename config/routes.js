eb52b6bff2ef5f4d829eaaf79afb5bee8b191bf1
Henry Lee <hlee.0323@gmail.com>
2023-06-22 09:39:01 +0100

    added missing semicolons and fixed a typo

--- config/routes.js
+++ config/routes.js
@@ -10,14 +10,14 @@ const cancelOrdersController = require('../controllers/cancelOrdersController');
 
 const routes = (app) => {
     app.get('/products', getProductsController.getProducts); // Get all products
-    app.post('/products', addProductsController.addProducts); // Add a products
+    app.post('/products', addProductsController.addProducts); // Add a product
     app.put('/products/:SKU', updateProductsController.updateProducts); // Update a product
     app.delete('/products/:SKU', deleteProductsController.deleteProducts); // Delete a product
     app.get('/products/:SKU', getStockLevelsByIdController.getStockLevelsById); // Get stock levels
     app.put('/update/:SKU', updateStockLevelsController.updateStockLevels); // Update stock levels
     app.get('/orders', getOrdersController.getOrders); // Get all orders
     app.post('/orders', createOrdersController.createOrder); // Add order
-    app.put('/orders/:orderNumber', cancelOrdersController.cancelOrders) // Cancel order
+    app.put('/orders/:orderNumber', cancelOrdersController.cancelOrders); // Cancel order
 }
 
 module.exports = routes;
